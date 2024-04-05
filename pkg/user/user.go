0ef6f726f3062972473a508f9d1c79a0db9b0890
Luke Ward <lbw4459@rit.edu>
2024-04-04 21:04:46 -0400

    added fetch user stuff

--- pkg/user/user.go
+++ pkg/user/user.go
@@ -1,1 +1,46 @@
 package user
+
+import (
+	"github.com/aws/aws-lambda-go/events"
+	"errors"
+	"encoding/json"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbiface"
+)
+
+var ()
+
+// you can use structs as the model
+// which allows you to put models and controllers in the same file
+
+type User struct {
+	Email     string `json:"email`
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+}
+
+func FetchUser(email, tableName string, dynaClient dynamodbiface.DynamoDBAPI)(*User, error) {
+	input := &dynamodb.GetItemInput{
+		Key: map[string]*dynamodb.AttributeValue{
+			"email":{
+				S: aws.String(email)
+			}
+		}
+	}
+}
+
+func FetchUsers() {
+
+}
+
+func CreateUser() {
+
+}
+
+func UpdateUser() {
+
+}
+
+func DeleteUser() error {
+
+}
